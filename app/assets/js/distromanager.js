66744730905a9bc2cbacecab68cdcf1edda3d6e9
ci2bx <74463191+ci2bx@users.noreply.github.com>
2024-06-11 11:05:35 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./configmanager')
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 // exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://lunexmc.github.io/LunexDistri/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://ci2bx.github.io/MinecraftForUnowhy-distri/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
